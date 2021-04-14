741d8de3b4cefe3416a739d72038a61918f19994
Grimnir09 <hovisjeremy9@gmail.com>
2021-04-14 17:08:34 -0500

    Casting int to roll args, Added Avery Check.

--- cogs/games.py
+++ cogs/games.py
@@ -3,17 +3,25 @@ from discord.ext import commands
 import discord
 from random import randint
 
+
+
 class games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(pass_context=True, description="Roll dice, ex. <!roll 1 20> for a 1d20")
     @commands.guild_only()
-    async def roll(self, ctx, dice, sides):
+
+    async def roll(self, ctx, dice:int, sides:int):
+        #idiot rangler
+        if dice > 20:
+            await ctx.reply("Sorry Limit is `20d`. Thank you Avery.")
+            return
+
         total = 0
-        rolls_left = int(dice)
+        rolls_left = dice
         while rolls_left != 0:
-            total += randint(1, int(sides))
+            total += randint(1, sides)
             rolls_left = rolls_left - 1
 
         await ctx.reply(f':game_die: Rolling a {dice}d{sides}... \n `{total}`', mention_author=False)
@@ -29,6 +37,7 @@ class games(commands.Cog):
         if isinstance(error, commands.errors.NoPrivateMessage):
             await ctx.send(f"{ctx.author.mention} {error}")
             return
+
 
 def setup(bot):
     bot.add_cog(games(bot))
